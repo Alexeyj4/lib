34ca968d0a6acc58e28b0eff58b2f2c0f21da434
Alexey <alexeyj4@yandex.ru>
2023-11-01 21:42:48 +0300

    adafruit constructor fixed

--- arduino/libraries/Oled/Oled.cpp
+++ arduino/libraries/Oled/Oled.cpp
@@ -1,18 +1,16 @@
 #include "Oled.h"
 
 Oled::Oled(){
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
   screen_width=128;
   screen_height=64;            
   oled_string_pos[0]=12; oled_string_pos[1]=28; oled_string_pos[2]=44; oled_string_pos[3]=62;
+  display=Adafruit_SSD1306(screen_width, screen_height, &Wire, -1); // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)  
   oled_text[0]=""; oled_text[1]=""; oled_text[2]=""; oled_text[3]=""; 
   oled_str_changed[0]=0; oled_str_changed[1]=0; oled_str_changed[2]=0; oled_str_changed[3]=0; 
   oled_need_update=0;
 }   
   
-void Oled::begin(){
-  Adafruit_SSD1306 display(screen_width, screen_height, &Wire, -1); // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)  
+void Oled::begin(){ 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setFont(&CourierCyr8pt8b);
   display.setTextSize(1);             
@@ -20,16 +18,16 @@ void Oled::begin(){
   display.clearDisplay();
   display.setCursor(0,oled_string_pos[0]); 
   display.println("loading OS...");
-  display.display();
-  delay(1000);       
+  display.display(); 
+  delay(500);       
 }
 
 void Oled::update(){
-  if(oled_need_update){
-    display.display();
+  if(oled_need_update){	  
+	display.fillRect(0,0,screen_width,screen_height,BLACK); 
     for(int i=0;i<=3;i++){
       if(oled_str_changed[i]){
-        display.setCursor(0,oled_string_pos[0]);
+        display.setCursor(0,oled_string_pos[i]);
         display.print(oled_text[i]);
         oled_str_changed[i]=0;
       }
--- arduino/libraries/Oled/Oled.h
+++ arduino/libraries/Oled/Oled.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <CourierCyr8.h>
@@ -9,8 +8,7 @@ class Oled{
     Oled();
     void begin();
     void update();
-    void print(int row, String s);  
-      
+    void print(int row, String s);        
     int screen_width; // OLED display width, in pixels
     int screen_height; // OLED display height, in pixels    
     int oled_string_pos[4];//positon for each string on oled
