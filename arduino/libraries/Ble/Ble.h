79d754a1d97a6d530b4876e9c1b1a0ba5c32e868
Vlad <Klisunov.vladdi94@gmail.com>
2023-11-30 14:10:33 +0300

    Update Ble.h

--- arduino/libraries/Ble/Ble.h
+++ arduino/libraries/Ble/Ble.h
@@ -3,14 +3,16 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <arduino.h>
 
 class Ble{
-  public:
+  public:	 
     Ble();
+	String s;
 	void begin();
 	bool send(String s);//1-ok //0-not ok
 	String recv();//""-not to received	
 	bool deviceConnected;
 	BLECharacteristic *pCharacteristic;
-	String s;
+	
 };
